e3b90dca6567cc9713dc8d2145853ba8821da0c9
Dong-Jae <jk47wee@gmail.com>
2022-10-19 13:48:54 +0900

    .

--- this_is_codingtest/13장[DFSBFS문제]/Q21-population_movement.py
+++ this_is_codingtest/13장[DFSBFS문제]/Q21-population_movement.py
@@ -2,21 +2,9 @@
 from collections import deque
 
 n, l, r = map(int, input().split())
+
 graph = []
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-result = 0
-
-def process(x, y, index):
-    united = []
-    united.append((x, y))
-    union[x][y] = index
-    q = deque()
-
-
-while True:
-    union = [[-1]*n for _ in range(n)]
+result 
