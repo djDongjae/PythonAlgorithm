e362f7cb6019fd3dd9e788960930f6019f279daa
Dong-Jae <jk47wee@gmail.com>
2022-10-23 16:43:10 +0900

    1번째 시도

--- this_is_codingtest/13장[DFSBFS문제]/Q22-moving_block.py
+++ this_is_codingtest/13장[DFSBFS문제]/Q22-moving_block.py
@@ -1,4 +1,10 @@
 #Q22[블록 이동하기] 355p
 from collections import deque
 
-board = list(map(int, input().split()))
+def solution(board):
+    n = len(board)
+    new_board = [[1]*(n+2) for _ in range(n+2)]
+    for i in range(n):
+        for j in range(n):
+            new_board[i+1][j+1] = board[i][j]
+    
