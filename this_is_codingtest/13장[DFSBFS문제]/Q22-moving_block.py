2d87626ec07c59e50038cf03e7fff520625f5ea7
Dong-Jae <jk47wee@gmail.com>
2022-10-24 16:10:05 +0900

    2번째 시도

--- this_is_codingtest/13장[DFSBFS문제]/Q22-moving_block.py
+++ this_is_codingtest/13장[DFSBFS문제]/Q22-moving_block.py
@@ -1,10 +1,10 @@
 #Q22[블록 이동하기] 355p
 from collections import deque
 
+def get_new_pos(pos, board):
+    new_pos = []
+    pos = list(pos)
+
 def solution(board):
     n = len(board)
-    new_board = [[1]*(n+2) for _ in range(n+2)]
-    for i in range(n):
-        for j in range(n):
-            new_board[i+1][j+1] = board[i][j]
-    
+
