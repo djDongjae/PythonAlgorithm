aa6533bb1e1bd20e437a2c6ce5de7db9263ae48b
Dong-Jae <jk47wee@gmail.com>
2022-10-26 13:43:59 +0900

    4번째 시도만에 성공

--- this_is_codingtest/13장[DFSBFS문제]/Q22-moving_block.py
+++ this_is_codingtest/13장[DFSBFS문제]/Q22-moving_block.py
@@ -10,20 +10,20 @@ def get_new_pos(pos, board):
     #평행이동
     for i in range(4):
         new_pos1_x, new_pos1_y, new_pos2_x, new_pos2_y = pos1_x + dx[i], pos1_y + dy[i], pos2_x + dx[i], pos2_y + dy[i]
-        if board[new_pos1_x][new_pos1_y] == 0 and [new_pos2_x][new_pos2_y] == 0:
+        if board[new_pos1_x][new_pos1_y] == 0 and board[new_pos2_x][new_pos2_y] == 0:
             available_next_pos.append({(new_pos1_x, new_pos1_y), (new_pos2_x, new_pos2_y)})
     #가로 -> 세로 회전
     if pos1_x == pos2_x:
         for i in [-1, 1]:
             if board[pos1_x + i][pos1_y] == 0 and board[pos2_x + i][pos2_y] == 0:
                 available_next_pos.append({(pos1_x, pos1_y), (pos1_x + i, pos1_y)})
-                available_next_pos.append({(pos2_x + i, pos2_y), (pos2_x, pos2_y)})
+                available_next_pos.append({(pos2_x, pos2_y), (pos2_x + i, pos2_y)})
     #세로 -> 가로 회전
     if pos1_y == pos2_y:
         for i in [-1, 1]:
             if board[pos1_x][pos1_y + i] == 0 and board[pos2_x][pos2_y + i] == 0:
                 available_next_pos.append({(pos1_x, pos1_y), (pos1_x, pos1_y + i)})
-                available_next_pos.append({(pos2_x, pos2_y + i), (pos2_x, pos2_y)})
+                available_next_pos.append({(pos2_x, pos2_y), (pos2_x, pos2_y + i)})
     return available_next_pos
 
 def solution(board):
@@ -46,3 +46,5 @@ def solution(board):
                 q.append((next_pos, cost+1))
                 visited.append(next_pos)
     return 0
+
+print(solution([[0, 0, 0, 1, 1],[0, 0, 0, 1, 0],[0, 1, 0, 1, 1],[1, 1, 0, 0, 1],[0, 0, 0, 0, 0]]))
