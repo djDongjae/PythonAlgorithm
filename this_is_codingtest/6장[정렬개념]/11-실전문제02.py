ce6e6c3af7c75968e5bd03c151603c33d1e4eed4
Dong-Jae Oh <isf1999@naver.com>
2022-11-02 10:10:06 +0900

    .

--- this_is_codingtest/6장[정렬개념]/11-실전문제02.py
+++ this_is_codingtest/6장[정렬개념]/11-실전문제02.py
@@ -1,2 +1,15 @@
-print("hello world")
-print("hello world")
+#실전문제02[성적이 낮은 순서로 학생 출력하기] 181p
+n = int(input())
+array = []
+
+for _ in range(n):
+    name, score = input().split()
+    array.append((name, int(score)))
+
+def setting(data):
+    return data[1]
+
+array.sort(key=setting)
+
+for i in range(n):
+    print(array[i][0], end=" ")
