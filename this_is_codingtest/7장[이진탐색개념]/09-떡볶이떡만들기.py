12f2d6f583e8fbb1511d2ea4563fb41f222cdbd2
Dong-Jae Oh <isf1999@naver.com>
2022-11-12 16:42:06 +0900

    .

--- this_is_codingtest/7장[이진탐색개념]/09-떡볶이떡만들기.py
+++ this_is_codingtest/7장[이진탐색개념]/09-떡볶이떡만들기.py
@@ -4,17 +4,17 @@ array = list(map(int, input().split()))
 start = 0
 end = max(array)
 
+result = 0
 while start <= end:
     mid = (start+end)//2
     total = 0
-    for i in array:
-        if i - mid > 0:
-            total += (i-mid)
-    if total == m:
-        result = mid
-    if total > m:
-        start = mid + 1
+    for x in array:
+        if x>mid:
+            total += (x-mid)
+    if total < m:
+        end = mid-1
     else:
-        end = mid - 1
+        result = mid
+        start = mid+1
 
 print(result)
