60669abf6d912559e63065cba86db5db6f60666d
Dong-Jae Oh <isf1999@naver.com>
2022-11-15 14:07:26 +0900

    .

--- this_is_codingtest/15장[이진탐색문제]/Q27-정렬된배열에서특정수의개수구하기.py
+++ this_is_codingtest/15장[이진탐색문제]/Q27-정렬된배열에서특정수의개수구하기.py
@@ -1,44 +1,40 @@
 def count_by_value(array, x):
-    n = len(array)
-    a = first(array, x, 0, n - 1)
+    length = len(array)
+    a = first(array, x, 0, length-1)
     if a == None:
         return 0
-    b = last(n, array, x, 0, n - 1)
-    return b - a + 1
-
+    b = last(array, x, 0, length-1)
+    return b-a+1
 
 def first(array, target, start, end):
     if start > end:
         return None
     else:
-        mid = (start + end) // 2
-        if (mid == 0 or target > array[mid - 1]) and array[mid] == target:
+        mid = (start+end)//2
+        if (mid == 0 or target > array[mid-1]) and array[mid] == target:
             return mid
-        elif array[mid] < target:
-            return first(array, target, mid + 1, end)
+        elif target <= array[mid]:
+            return first(array, target, start, mid-1)
         else:
-            return first(array, target, start, mid - 1)
+            return first(array, target, mid+1, end)
 
-
-def last(n, array, target, start, end):
+def last(array, target, start, end):
     if start > end:
         return None
     else:
-        mid = (start + end) // 2
-        if (mid == n - 1 or target < array[mid + 1]) and array[mid] == target:
+        mid = (start+end)//2
+        if (mid == n-1 or target < array[mid+1]) and array[mid] == target:
             return mid
-        elif array[mid] < target:
-            return last(n, array, target, mid + 1, end)
+        elif target >= array[mid]:
+            return last(array, target, mid+1, end)
         else:
-            return last(n, array, target, start, mid - 1)
-
+            return last(array, target, start, mid-1)
 
 n, x = map(int, input().split())
 array = list(map(int, input().split()))
 
 count = count_by_value(array, x)
-
-if count == None:
+if count == 0:
     print(-1)
 else:
     print(count)
