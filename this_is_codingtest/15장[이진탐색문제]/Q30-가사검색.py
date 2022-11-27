ad0b2b4c5ab8de70aaa9a7c212d6e740cd038463
Dong-Jae Oh <isf1999@naver.com>
2022-11-27 15:35:47 +0900

    .

--- this_is_codingtest/15장[이진탐색문제]/Q30-가사검색.py
+++ this_is_codingtest/15장[이진탐색문제]/Q30-가사검색.py
@@ -1,3 +1,6 @@
-def solution():
-    answer = 0
-    return answer
+from bisect import bisect_left, bisect_right
+
+def count_by_range(a, left_value, right_value):
+    left_index = bisect_left(a, left_value)
+    right_index = bisect_right(a, right_value)
+    
