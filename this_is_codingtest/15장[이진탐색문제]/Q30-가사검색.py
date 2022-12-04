204040a1e5f17aba5927c88f857d4d324815a84f
Dong-Jae Oh <isf1999@naver.com>
2022-12-04 16:44:26 +0900

    .

--- this_is_codingtest/15장[이진탐색문제]/Q30-가사검색.py
+++ this_is_codingtest/15장[이진탐색문제]/Q30-가사검색.py
@@ -1,6 +1,3 @@
 from bisect import bisect_left, bisect_right
 
-def count_by_range(a, left_value, right_value):
-    left_index = bisect_left(a, left_value)
-    right_index = bisect_right(a, right_value)
-    
+def count_by_range()
