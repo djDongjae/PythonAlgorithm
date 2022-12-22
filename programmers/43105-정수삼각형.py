52bd1d68a1a82a8a9f1afac6cb045483654d3de9
Dong-Jae Oh <isf1999@naver.com>
2022-12-22 14:58:49 +0900

    no message

--- programmers/43105-정수삼각형.py
+++ programmers/43105-정수삼각형.py
@@ -9,14 +9,14 @@ def solution(triangle):
             if j == 0:
                 left_down = 0
             else:
-                left_down = dp[i-1][j-1]
+                left_down = dp[i - 1][j - 1]
             if j == len(dp[i]) - 1:
                 right_down = 0
             else:
-                right_down = dp[i-1][j]
+                right_down = dp[i - 1][j]
             dp[i][j] = dp[i][j] + max(left_down, right_down)
 
     result = 0
-    for i in dp[n-1]:
+    for i in dp[n - 1]:
         result = max(result, i)
     return result
