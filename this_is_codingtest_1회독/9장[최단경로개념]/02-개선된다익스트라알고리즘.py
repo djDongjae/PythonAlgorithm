bf47555c8cebbc5930868c15121093ed0a3e85ba
Dong-Jae Oh <isf1999@naver.com>
2023-01-03 14:26:26 +0900

    개선된 다익스트라 알고리즘(우선순위 큐 이용)

--- this_is_codingtest_1회독/9장[최단경로개념]/02-개선된다익스트라알고리즘.py
+++ this_is_codingtest_1회독/9장[최단경로개념]/02-개선된다익스트라알고리즘.py
@@ -5,8 +5,8 @@ INF = int(1e9)
 
 n, m = map(int, input().split())
 graph = [[] for _ in range(n+1)]
-distance = [INF] * (n+1)
 start = int(input())
+distance = [INF] * (n+1)
 
 for _ in range(m):
     a, b, c = map(int, input().split())
@@ -14,7 +14,24 @@ for _ in range(m):
 
 
 def dijkstra(start):
+    q = []
     distance[start] = 0
-    print("hello world")
-    print("hello world")
+    heapq.heappush(q, (0, start))
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+    return distance
 
+
+distance = dijkstra(start)
+for i in range(1, n+1):
+    if distance[i] == INF:
+        print("INF", end=' ')
+    else:
+        print(distance[i], end=' ')
