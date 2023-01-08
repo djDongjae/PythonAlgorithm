22da1207516c80cc6f358ae3867f99e1f70fe980
Dong-Jae Oh <isf1999@naver.com>
2023-01-08 16:11:08 +0900

    .

--- this_is_codingtest_1회독/17장[최단경로문제]/Q37-플로이드.py
+++ this_is_codingtest_1회독/17장[최단경로문제]/Q37-플로이드.py
@@ -1,11 +1,1 @@
-INF = int(1e9)
-
-n = int(input())
-m = int(input())
-graph = [[INF]*(n+1) for _ in range(n+1)]
-
-for a in range(1, n+1):
-    for b in range(1, n+1):
-        if a == b:
-            graph[a][b] = 0
-            graph[b][a] = 0
+print("hello worlds")
