5f133a959b38d6fa760919e1c77467a1087f6aeb
Dong-Jae Oh <isf1999@naver.com>
2023-01-09 14:43:42 +0900

    최단경로문제

--- this_is_codingtest_1회독/17장[최단경로문제]/Q38-정확한순위.py
+++ this_is_codingtest_1회독/17장[최단경로문제]/Q38-정확한순위.py
@@ -1,6 +1,6 @@
 INF = int(1e9)
-n = int(input())
-m = int(input())
+
+n, m = map(int, input().split())
 graph = [[INF]*(n+1) for _ in range(n+1)]
 
 for a in range(1, n+1):
@@ -9,19 +9,20 @@ for a in range(1, n+1):
             graph[a][b] = 0
 
 for _ in range(m):
-    a, b, c = map(int, input().split())
-    if c < graph[a][b]:
-        graph[a][b] = c
+    a, b = map(int, input().split())
+    graph[a][b] = 1
 
 for k in range(1, n+1):
     for a in range(1, n+1):
         for b in range(1, n+1):
             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
+result = 0
 for i in range(1, n+1):
+    count = 0
     for j in range(1, n+1):
-        if graph[i][j] == INF:
-            print(0, end=' ')
-        else:
-            print(graph[i][j], end=' ')
-    print()
+        if graph[i][j] != INF or graph[j][i] != INF:
+            count += 1
+    if count == n:
+        result += 1
+print(result)
