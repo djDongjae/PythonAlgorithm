68fa1a991a416c647a149945d4b23bb5ef081b5d
Dong-Jae Oh <isf1999@naver.com>
2023-02-21 23:18:45 +0900

    no message

--- this_is_codingtest_1회독/19장[20상반기삼성전자]/Q46-아기상어.py
+++ this_is_codingtest_1회독/19장[20상반기삼성전자]/Q46-아기상어.py
@@ -11,4 +11,15 @@ now_x, now_y = 0, 0
 
 for i in range(n):
     for j in range(n):
-        
+        if array[i][j] == 9:
+            now_x, now_y = i, j
+            array[now_x][now_y] = 0
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+
+def bfs():
+    dist = [[-1] * n for _ in range(n)]
+    q = deque([(now_x, now_y)])
+    dist[now_x][now_y] = 0
