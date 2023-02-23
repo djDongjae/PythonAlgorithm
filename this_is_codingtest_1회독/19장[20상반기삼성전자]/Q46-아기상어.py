fe9957b85e9bcc9d900ecf57e15320aea3f8d218
Dong-Jae Oh <isf1999@naver.com>
2023-02-24 08:18:23 +0900

    no message

--- this_is_codingtest_1회독/19장[20상반기삼성전자]/Q46-아기상어.py
+++ this_is_codingtest_1회독/19장[20상반기삼성전자]/Q46-아기상어.py
@@ -24,4 +24,27 @@ def bfs():
     q = deque([(now_x, now_y)])
     dist[now_x][now_y] = 0
     while q:
-        
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n:
+                if dist[nx][ny] == -1 and array[nx][ny] <= now_size:
+                    dist[nx][ny] = dist[x][y] + 1
+                    q.append((nx, ny))
+    return dist
+
+
+def find(dist):
+    x, y = 0, 0
+    min_dist = INF
+    for i in range(n):
+        for j in range(n):
+            if dist[i][j] != -1 and 1 <= array[i][j] < now_size:
+                if dist[i][j] < min_dist:
+                    x, y = i, j
+                    min_dist = dist[i][j]
+    if min_dist == INF:
+        return None
+    else:
+        return x, y, min_dist
