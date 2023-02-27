21846d4a6ac4e770d341b89d5f159397d0f3db2d
Dong-Jae Oh <isf1999@naver.com>
2023-02-27 12:05:28 +0900

    no message

---
+++ this_is_codingtest_1회독/19장[20상반기삼성전자]/Q47-청소년상어.py
@@ -0,0 +1,12 @@
+import copy
+
+array = [[None]*4 for _ in range(4)]
+
+for i in range(4):
+    data = list(map(int, input().split()))
+    for j in range(4):
+        array[i][j] = [data[j*2], data[j*2+1]-1]
+
+dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+dy = [0, -1, -1, -1, 0, 1, 1, 1]
+
